31ddd8064d45dd2702351b513734bf6f3db2746c
hmud0721@naver.com <hmud0721@naver.com>
2025-09-12 15:03:43 +0900

    [feat] 싱긅톤 구현

---
+++ 유틸리티/싱글톤/Singleton.cs
@@ -0,0 +1,45 @@
+﻿namespace inonego
+{
+    // ==================================================================
+    /// <summary>
+    /// 싱글톤을 구현하기 위한 추상 클래스입니다.
+    /// </summary>
+    // ==================================================================
+    public abstract class Singleton<T> where T : Singleton<T>, new()
+    {
+
+    #region 필드
+
+        private static T instance;
+
+        // ------------------------------------------------------------
+        /// <summary>
+        /// 싱글톤 인스턴스를 반환합니다.
+        /// </summary>
+        // ------------------------------------------------------------
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (typeof(T))
+                    {
+                        if (instance == null)
+                        {
+                            instance = new();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        public static void ClearInstance() => instance = null;
+
+    #endregion
+
+    }
+
+}

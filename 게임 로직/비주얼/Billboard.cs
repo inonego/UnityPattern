ffd45d51eae13008eaefca1b2974642b5771f548
hmud0721@naver.com <hmud0721@naver.com>
2025-10-12 19:59:23 +0900

    [feat] 머리 위에 UI가 고정되도록 하는 HeadUpFollow 스크립트 작성

--- 게임 로직/비주얼/Billboard.cs
+++ 게임 로직/비주얼/Billboard.cs
@@ -1,48 +1,54 @@
+using System;
+
 using UnityEngine;
 
 namespace inonego
 {
+    using inonego.Serializable;
+
+    public enum BillboardLookMode
+    {
+        Parallel, HardLook,
+    }
+    
     [DefaultExecutionOrder(1000)]
     public class Billboard : MonoBehaviour
     {
-        public enum LookMode
-        {
-            Parallel, HardLook,
-        }
-
         public Transform OverrideLookTarget = null;
-        public LookMode Mode = LookMode.Parallel;
-        public Vector3 Up = Vector3.up;
+        public XNullable<BillboardLookMode> OverrideMode = null;
+        public XNullable<Vector3> OverrideUp = null;
 
         private void LateUpdate()
         {
             var lookTarget = OverrideLookTarget != null ? OverrideLookTarget : BillboardManager.Instance.LookTarget;
+            var mode = OverrideMode.HasValue ? OverrideMode.Value : BillboardManager.Instance.Mode;
+            var up = OverrideUp.HasValue ? OverrideUp.Value : BillboardManager.Instance.Up;
 
-            switch (Mode)
+            switch (mode)
             {
-                case LookMode.Parallel:
-                    SetLookModeParallel(lookTarget);
+                case BillboardLookMode.Parallel:
+                    SetLookModeParallel(lookTarget, up);
                     break;
-                case LookMode.HardLook:
-                    SetLookModeHardLook(lookTarget);
+                case BillboardLookMode.HardLook:
+                    SetLookModeHardLook(lookTarget, up);
                     break;
             }
         }
 
-        private void SetLookModeParallel(Transform lookTarget)
+        private void SetLookModeParallel(Transform lookTarget, Vector3 up)
         {
             if (lookTarget == null) return;
 
-            transform.rotation = Quaternion.LookRotation(lookTarget.forward, Up);
+            transform.rotation = Quaternion.LookRotation(lookTarget.forward, up);
         }
 
-        private void SetLookModeHardLook(Transform lookTarget)
+        private void SetLookModeHardLook(Transform lookTarget, Vector3 up)
         {
             if (lookTarget == null) return;
 
             Vector3 delta = (lookTarget.position - transform.position).normalized;
 
-            transform.rotation = Quaternion.LookRotation(delta, Up);
+            transform.rotation = Quaternion.LookRotation(delta, up);
         }
     }
 }
--- 게임 로직/비주얼/BillboardManager.cs
+++ 게임 로직/비주얼/BillboardManager.cs
@@ -4,6 +4,8 @@ namespace inonego
 {
     public class BillboardManager : MonoSingleton<BillboardManager>
     {
-        public Transform LookTarget = null;
+        public Transform LookTarget;
+        public BillboardLookMode Mode = BillboardLookMode.Parallel;
+        public Vector3 Up = Vector3.up;
     }
 }
